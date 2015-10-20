5f0a209c2c7176ef8c046ec915a568daafcbb986
BlockoS <cruz.vincent@gmail.com>
2015-10-20 20:51:31 +0000

    Added comments.

--- dmf.cpp
+++ dmf.cpp
@@ -3,14 +3,18 @@
 #include "dmf.h"
 
 namespace DMF {
-    bool isEmpty(PatternData const& src)
-    {
-        return    (src.octave == 0)
-               && (src.note == 0)
-               && (src.instrument == 0xffff)
-               && (src.volume == 0xffff)
-               && (src.effect[0].code == 0xffff)
-               && (src.effect[0].data == 0xffff);
-    }
+    
+/// Check if a pattern element is empty. 
+/// @param [in] src Pattern data.
+/// @return true if the pattern element is empty, false otherwise.
+bool isEmpty(PatternData const& src)
+{
+    return    (src.octave == 0)
+           && (src.note == 0)
+           && (src.instrument == 0xffff)
+           && (src.volume == 0xffff)
+           && (src.effect[0].code == 0xffff)
+           && (src.effect[0].data == 0xffff);
+}
 
 } // DMF
--- dmf.h
+++ dmf.h
@@ -59,7 +59,7 @@ enum FramesMode
 };
 
 /// Effect list.
-enum Effect
+enum Effect : uint8_t
 {
     // Standard Commands
     ARPEGGIO                            = 0x00,
@@ -93,86 +93,151 @@ enum Effect
     SET_SAMPLES     = 0x17
 };
 
+/// DMF fixed size string.
 struct String
 {
+    /// String length.
     uint8_t length;
-    char    data[MAX_STRING_BUFFER_LEN];
+    /// String data.
+    char data[MAX_STRING_BUFFER_LEN];
 }; 
 
+/// DMG song infos.
 struct Infos
 {
+    /// DMF version.
     uint8_t version;
+    /// Target system.
     uint8_t system;
+    /// Song name.
     String  name;
+    /// Author.
     String  author;
+    /// Rows lines highlighting.
     uint8_t highlight[2];
+    /// The base time value multiplies the tick time value.
     uint8_t timeBase;
+    /// Tick time.
     uint8_t tickTime[2];
+    /// Frames mode.
     uint8_t framesMode;
+    /// Custom frequency flag.
     uint8_t customFreqFlag;
+    /// Custom frequency value.
     uint8_t customFreqValue[3];
+    /// Number of rows per pattern.
     uint8_t totalRowsPerPattern;
+    /// Total number of rows in the pattern matrix.
     uint8_t totalRowsInPatternMatrix;
+    /// Speed of the arpeggio command.
     uint8_t arpeggioTickSpeed;
+    /// Number of channels.
     uint8_t systemChanCount;
 };
 
+/// Envelope.
 struct Envelope
 {
+    /// Length.
     uint8_t size;
+    /// Loop index.
     uint8_t loop;
+    /// Data.
     uint8_t value[4*128];
 };
 
+/// Instrument.
+/// @note Only standard instruments are supported for the moment.
 struct Instrument
 {
+    /// Standard instrument.
     struct Standard
-    {   
+    {
+        /// Volume envelope.
         Envelope volume;
+        /// Arpeggio (tone envelope).
         Envelope arpeggio;
+        /// Arpeggio mode.
+        ///   * normal = 0
+        ///   * fixed = 1
         uint8_t  arpeggioMode;
+        /// Noise envelope.
         Envelope noise;
+        /// Wavetable macro.
         Envelope wave;
     };
-
+    /// Name.
     String   name;
+    /// Mode.
+    ///    * standard = 0
+    ///    * FM = 1
     uint8_t  mode;
+    /// Standard instrument data.
     Standard std; 
 };
 
+/// Wavetable.
 typedef std::vector<uint32_t> WaveTable;
 
+/// Pattern data.
 struct PatternData
 {
-    uint16_t note;
-    uint16_t octave;
-    uint16_t volume;
-    struct
+    /// Effect.
+    struct Effect
     {
+        /// Code.
         uint16_t code;
+        /// Data.
         uint16_t data;
-    } effect[DMF_MAX_EFFECT_COUNT];
+    };
+    /// Note. 
+    ///   * [1,12] = {C#,D ,D#,E ,F ,F#,G ,G#,A ,A#,B ,C }.
+    ///   * 0x100 = note off.
+    uint16_t note;
+    /// Octave.
+    uint16_t octave;
+    /// Volume.
+    uint16_t volume;
+    /// Effects.
+    Effect effect[DMF_MAX_EFFECT_COUNT];
+    /// Instrument index.
     uint16_t instrument;
 };
 
+/// Check if a pattern element is empty. 
+/// @param [in] src Pattern data.
+/// @return true if the pattern element is empty, false otherwise.
 bool isEmpty(PatternData const& src);
 
+/// PCM sample.
 struct Sample
 {
+    /// Sample rate.
     uint8_t  rate;
+    /// Pitch.
     uint8_t  pitch;
+    /// Amp.
     uint8_t  amp;
+    /// Sample data.
     std::vector<uint16_t> data;
 };
 
+/// DMF Song.
 struct Song
 {
+    /// Infos.
     Infos  infos;
+    /// Pattern matrix.
     std::vector<uint8_t> patternMatrix;
+    /// Instruments.
     std::vector<Instrument> instrument;
+    /// Wave tables.
     std::vector<WaveTable> waveTable;
+    /// Number of effects per pattern.
     std::vector<uint8_t> effectCount;
+    /// Patterns.
     std::vector<PatternData> patternData;
+    /// Samples.
     std::vector<Sample> sample;
 };
 
